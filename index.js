ae58de53f8b5ea27ec500ec3004bd7e9d351885f
georgesuarez <georgejsuarez@gmail.com>
2023-10-02 21:38:53 -0700

    Created generateFroyoOrder function

--- index.js
+++ index.js
@@ -1,38 +1,32 @@
 // Prompt the user for a list of froyo flavors
 const userInput = prompt(
-    "Please enter some froyo flavors seperated by commas.",
-    "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
+  "Please enter some froyo flavors seperated by commas.",
+  "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
 );
 
-// Split the string of froyo flavors into an array of objects.
-const stringArray = userInput.split(",");
+// Split the string of froyoFlavors seperated by commas
+const froyoFlavors = userInput.split(",");
 
-// Convert the array of strings into an array of objects.
 const froyoOrders = [];
-for (let i = 0; i < stringArray.length; i++) {
-    const froyoFlavor = stringArray[i];
-    const froyoOrder = {
-        order: {
-            flavor: froyoFlavor,
-            count: 1
-        },
-    };
-    froyoOrders.push(froyoOrder);
+// TODO: Fix this method
+function generateFroyoOrder() {
+  for (let i = 0; i < froyoFlavors.length; i++) {
+    if (froyoOrders.length === 0) {
+      froyoOrders.push({ flavor: froyoFlavors[i], count: 1 });
+    } else {
+      for (const order in froyoOrders) {
+        if (froyoFlavors[i] === froyoOrders[order].flavor) {
+          froyoOrders[order].count++;
+        } else {
+          froyoOrders.push({ flavor: froyoFlavors[i], count: 1 });
+        }
+      }
+    }
+  }
 }
+
+generateFroyoOrder();
 
 for (const order in froyoOrders) {
-    console.log(froyoOrders[order]);
+  console.log(froyoOrders[order]);
 }
-
-/**
- * @param {froyoOrders[]} froyoOrders an array of froyo objects
- * @param {froyoFlavor} froyoFlavor a string of a specific froyo flavor
- * @returns {number} the count of the froyo flavor 
- */
-// function getFroyoFlavorCount(froyoOrders, froyoFlavor) {
-//     let count = 0;
-//     for (const flavor in froyoOrders) {
-
-//     }
-// }
-

11ffa17f0dbbb71f6a345a731d3d278e238341b6
George Suarez <georgejsuarez@gmail.com>
2023-10-02 23:25:28 -0700

    Number of Froyo flavors is now being tracked correctly

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
 // Prompt the user for a list of froyo flavors
 const userInput = prompt(
-  "Please enter some froyo flavors seperated by commas.",
-  "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
+    "Please enter some froyo flavors seperated by commas.",
+    "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
 );
 
 // Split the string of froyoFlavors seperated by commas
 const froyoFlavors = userInput.split(",");
 
 const froyoOrders = [];
-// TODO: Fix this method
 function generateFroyoOrder() {
-  for (let i = 0; i < froyoFlavors.length; i++) {
-    if (froyoOrders.length === 0) {
-      froyoOrders.push({ flavor: froyoFlavors[i], count: 1 });
-    } else {
-      for (const order in froyoOrders) {
-        if (froyoFlavors[i] === froyoOrders[order].flavor) {
-          froyoOrders[order].count++;
-        } else {
-          froyoOrders.push({ flavor: froyoFlavors[i], count: 1 });
-        }
-      }
+    for (let i = 0; i < froyoFlavors.length; i++) {
+        froyoOrders.push({
+            flavor: froyoFlavors[i],
+            count: countFroyoFlavors(froyoFlavors[i]),
+        });
     }
-  }
+}
+
+function countFroyoFlavors(target) {
+    let count = 0;
+    for (let i = 0; i < froyoFlavors.length; i++) {
+        if (froyoFlavors[i] === target) {
+            count++;
+        }
+    }
+    return count;
+}
+
+function displayFroyoOrder() {
+    for (const order in froyoOrders) {
+        console.log(
+            `Flavor: ${froyoOrders[order].flavor} Count: ${froyoOrders[order].count}`
+        );
+    }
 }
 
 generateFroyoOrder();
-
-for (const order in froyoOrders) {
-  console.log(froyoOrders[order]);
-}
+displayFroyoOrder();
